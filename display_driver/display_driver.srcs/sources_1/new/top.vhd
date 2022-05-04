962f0cd8030a9ad921d22a2c8541e6165328f29e
Matúš Bohucký <58601109+bohus98@users.noreply.github.com>
2022-05-04 11:50:33 +0200

    Update top.vhd

--- display_driver/display_driver.srcs/sources_1/new/top.vhd
+++ display_driver/display_driver.srcs/sources_1/new/top.vhd
@@ -1,22 +1,4 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/29/2022 04:17:58 PM
--- Design Name: 
--- Module Name: top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+
 
 
 library IEEE;
@@ -98,11 +80,9 @@ begin
           data7_i(1) => SW(13),
           data7_i(0) => SW(12),
           
-          --dp_i       => "0111",
+           
           
-          --dp_o       => DP,    
-          
-          -- MAP DECIMAL POINT AND DISPLAY SEGMENTS
+          -- MAP  DISPLAY SEGMENTS
           seg_o(6) => CA,
           seg_o(5) => CB,
           seg_o(4) => CC,
@@ -114,7 +94,5 @@ begin
           dig_o(7 downto 0) => AN(7 downto 0)
       );
 
-  -- Disconnect the top four digits of the 7-segment display
-  --AN(7 downto 4) <= b"1111";
 
 end architecture Behavioral;
