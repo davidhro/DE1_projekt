614b7c5645235c3a828d5fe307d2d5f15d3b4592
Matúš Bohucký <58601109+bohus98@users.noreply.github.com>
2022-05-04 11:51:06 +0200

    Update hex_7seg.vhd

--- display_driver/display_driver.srcs/sources_1/new/hex_7seg.vhd
+++ display_driver/display_driver.srcs/sources_1/new/hex_7seg.vhd
@@ -1,13 +1,4 @@
-------------------------------------------------------------
---
--- Template for 7-segment display decoder.
--- Nexys A7-50T, Vivado v2020.1, EDA Playground
---
--- Copyright (c) 2018-Present Tomas Fryza
--- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
--- This work is licensed under the terms of the MIT license.
---
-------------------------------------------------------------
+
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -28,19 +19,7 @@ end entity hex_7seg;
 architecture Behavioral of hex_7seg is
 begin
     --------------------------------------------------------
-    -- p_7seg_decoder:
-    -- A combinational process for 7-segment display (Common
-    -- Anode) decoder. Any time "hex_i" is changed, the process
-    -- is "executed". Output pin seg_o(6) controls segment A,
-    -- seg_o(5) segment B, etc.
-    --       segment A
-    --        | segment B
-    --        |  | segment C
-    --        +-+|  |   ...   segment G
-    --          ||+-+          |
-    --          |||            |
-    -- seg_o = "0000001"-------+
-    --------------------------------------------------------
+
     p_7seg_decoder : process(hex_i)
     begin
         case hex_i is
