4e25afd5ed08047525c93319c5c899875cc2ed04
bohus98 <58601109+bohus98@users.noreply.github.com>
2022-05-04 11:31:59 +0200

    init

--- display_driver/display_driver.runs/impl_1/top.tcl
+++ display_driver/display_driver.runs/impl_1/top.tcl
@@ -17,7 +17,7 @@ proc create_report { reportName command } {
   }
 }
 namespace eval ::optrace {
-  variable script "D:/Documents/xsloto00/digital-electronics-1/labs/07-display_driver/display_driver/display_driver.runs/impl_1/top.tcl"
+  variable script "D:/DE1_projekt/display_driver/display_driver.runs/impl_1/top.tcl"
   variable category "vivado_impl"
 }
 
@@ -112,33 +112,32 @@ proc step_failed { step } {
   set endFile ".$step.error.rst"
   set ch [open $endFile w]
   close $ch
+OPTRACE "impl_1" END { }
 }
 
 
-OPTRACE "Implementation" START { ROLLUP_1 }
+OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Init Design" START { ROLLUP_AUTO }
 start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
-  set_param chipscope.maxJobs 1
-  set_param xicom.use_bs_reader 1
+  set_param chipscope.maxJobs 2
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7a50ticsg324-1L
-  set_property board_part digilentinc.com:nexys-a7-50t:part0:1.0 [current_project]
   set_property design_mode GateLvl [current_fileset]
   set_param project.singleFileAddWarning.threshold 0
 OPTRACE "create in-memory project" END { }
 OPTRACE "set parameters" START { }
-  set_property webtalk.parent_dir D:/Documents/xsloto00/digital-electronics-1/labs/07-display_driver/display_driver/display_driver.cache/wt [current_project]
-  set_property parent.project_path D:/Documents/xsloto00/digital-electronics-1/labs/07-display_driver/display_driver/display_driver.xpr [current_project]
-  set_property ip_output_repo D:/Documents/xsloto00/digital-electronics-1/labs/07-display_driver/display_driver/display_driver.cache/ip [current_project]
+  set_property webtalk.parent_dir D:/DE1_projekt/display_driver/display_driver.cache/wt [current_project]
+  set_property parent.project_path D:/DE1_projekt/display_driver/display_driver.xpr [current_project]
+  set_property ip_output_repo D:/DE1_projekt/display_driver/display_driver.cache/ip [current_project]
   set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "set parameters" END { }
 OPTRACE "add files" START { }
-  add_files -quiet D:/Documents/xsloto00/digital-electronics-1/labs/07-display_driver/display_driver/display_driver.runs/synth_1/top.dcp
+  add_files -quiet D:/DE1_projekt/display_driver/display_driver.runs/synth_1/top.dcp
 OPTRACE "read constraints: implementation" START { }
-  read_xdc D:/Documents/xsloto00/digital-electronics-1/labs/07-display_driver/display_driver/display_driver.srcs/constrs_1/new/nexys-a7-50t.xdc
+  read_xdc D:/DE1_projekt/display_driver/display_driver.srcs/constrs_1/new/nexys-a7-50t.xdc
 OPTRACE "read constraints: implementation" END { }
 OPTRACE "add files" END { }
 OPTRACE "link_design" START { }
@@ -295,34 +294,4 @@ if {$rc} {
 
 OPTRACE "route_design misc" END { }
 OPTRACE "Phase: Route Design" END { }
-OPTRACE "Phase: Write Bitstream" START { ROLLUP_AUTO }
-OPTRACE "write_bitstream setup" START { }
-start_step write_bitstream
-set ACTIVE_STEP write_bitstream
-set rc [catch {
-  create_msg_db write_bitstream.pb
-OPTRACE "read constraints: write_bitstream" START { }
-OPTRACE "read constraints: write_bitstream" END { }
-  catch { write_mem_info -force top.mmi }
-OPTRACE "write_bitstream setup" END { }
-OPTRACE "write_bitstream" START { }
-  write_bitstream -force top.bit 
-OPTRACE "write_bitstream" END { }
-OPTRACE "write_bitstream misc" START { }
-OPTRACE "read constraints: write_bitstream_post" START { }
-OPTRACE "read constraints: write_bitstream_post" END { }
-  catch {write_debug_probes -quiet -force top}
-  catch {file copy -force top.ltx debug_nets.ltx}
-  close_msg_db -file write_bitstream.pb
-} RESULT]
-if {$rc} {
-  step_failed write_bitstream
-  return -code error $RESULT
-} else {
-  end_step write_bitstream
-  unset ACTIVE_STEP 
-}
-
-OPTRACE "write_bitstream misc" END { }
-OPTRACE "Phase: Write Bitstream" END { }
-OPTRACE "Implementation" END { }
+OPTRACE "impl_1" END { }
